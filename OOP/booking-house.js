0c9f6cd25324313765d2ff684dc187165908c5bd
darko018 <darkokovacevic018@gmail.com>
2021-06-16 18:05:00 +0200

    'updating shoping list'

--- OOP/booking-house.js
+++ OOP/booking-house.js
@@ -1,100 +1,236 @@
-'use strict';
+"use strict";
+
 (function () {
 
-
-    function Country (name, odds, continent) {
-
-        if (!countryName || !odds || !continent) {
-            throw new Error('Country: All properties are required')
-          }
-          if (!(continent instanceof Continent)) {
-            throw new Error('Country: Invalid continent input')
-          }
-
-        this.name = name;
+    function Country(name, odds, continent) {
+        if (!name || !odds) {
+            throw new Error ("Please fill out all fields!")
+        }
+        this.countryName = name;
         this.odds = odds;
+        if (!continent)  {
+            throw new Error ("Please, fill out continent name!");
+        }
         this.continent = continent;
-    }
+    };
+
+    function Continent () {
+        this.EUROPE = "EU";
+        this.ASIA = "AS";
+        this.AFRICA = "AF";
+        this.SOUTHAMERICA = "SA";
+        this.NORTHAMERICA = "NA";
+        this.AUSTRALIA = "AU";
+    };
 
 
-    function Person (name, surname, dateOfBirth) {
-        
-        if (!name || !surname || !dateOfBirth) {
-            throw new Error('Person: All fields are required')
-          }
-
+    function Person (name, surname, date) {
+        if (!name || !surname || !date) {
+            throw new Error ("Please, fill out the required fields!")
+        }
         this.name = name;
         this.surname = surname;
-        this.dateOfBirth = new (dateOfBirth);
-
-        this.getData = function () {
+        this.dateOfBirth = new Date(date);
+        this.getMethod = function () {
+            var result = "";
             var day = this.dateOfBirth.getDate();
             var month = this.dateOfBirth.getMonth() + 1;
             var year = this.dateOfBirth.getFullYear();
-            var result = day + '.' + month + '.' + year;
-      
-            return [this.name, this.surname, result].join(' ');
-          }
-      
-          this.getAge = function () {
-            var currentYear = (new Date()).getFullYear();
-            return currentYear - this.dateOfBirth.getFullYear();
-          }
-    }
-
-
-    function Player (person, betAmount, country) {
-
-        if (!person || !(person instanceof Person)) {
-            throw new Error('Player: Invalid person input');
-          }
-          if (!country || !(country instanceof Country)) {
-            throw new Error('Player: Invalid country input');
-          }
-
-          this.person = person;
-          this.betAmount = betAmount;
-          this.country = country;
-    }
-
-
-    function Adress (country, city, postalCode, street, number) {
-
+            return result += this.name + " " + this.surname + ", " + day + "." + month + "." + year + ".";
+        };
+    };
+    
+    function Player (person, bet, country) {
+        if (!person || !bet) {
+            throw new Error ("All fiends are required!");
+        }
+        this.person = person;
+        this.betAmount = bet;
         if (!country || !(country instanceof Country)) {
-            throw new Error('Address: Invalid country input');
-          }
-
-          this.country = country;
-          this.city = city;
-          this.postalCode = postalCode;
-          this.street = street;
-          this.number = number;
+            throw new Error ("Country field is required!")
+        }
+        this.country = country;
+        this.getMethod = function () {
+            var abbr = abbrevation(this.country.countryName);
+            var expectedAmount = (this.country.odds * this.betAmount).toFixed(2);
+            var age = getAge(this.person.dateOfBirth);
+            return abbr + ", " + expectedAmount + " eur, " + this.person.name + " " + this.person.surname + ", " + age + " years";
+        }
     }
 
+    function Address (country, city, code, street, number) {
+        if (!country || !city || !code || !street || !number) {
+            throw new Error ("Please, insert your full address!")
+        }
+        this.country = country;
+        this.city = city;
+        this.postalCode = code;
+        this.street = street;
+        this.streetNumber = number;
+        this.getMethod = function () {
+            var country = abbrevation(this.country); 
+            return this.street + " " + this.streetNumber + ", " + this.postalCode + " " + this.city + ", " + country;
+        }
+    };
 
-    function BettingPlace (adress) {
-
+    function BettingPlace (address) {
         if (!address || !(address instanceof Address)) {
-            throw new Error('Betting Place: Invalid address input');
-          }
-      
-          this.listOfPlayers = [];
-          this.address = address;
-    }
+            throw new Error ("Please insert your address!");
+        }
+        this.address = address;
+        this.listOfPlayers = [];
+        this.addPlayer = function (player) {
+            this.listOfPlayers.push(player);
+        }
+        this.getMethod = function () {
+            var allBets = 0;
+            this.listOfPlayers.forEach(function (player) {
+                allBets += player.betAmount;
+            });
+            return this.address.street + ", " + this.address.postalCode + " " + this.address.city + ", sum of all bets: " + allBets + "eur";
+        }
+    };
 
-
-    function BettingHouse (competition, numberOfPlayers) {
-
+    function BettingHouse (competition) {
         this.competition = competition;
-        this.numberOfPlayers = numberOfPlayers;
-        this.list = [];
+        this.listOfBettingPlaces = [];
+        this.numbersOfPlayers = 0;
+        this.addBettingPlaces = function (place) {
+            this.listOfBettingPlaces.push(place);
+            this.numbersOfPlayers += place.listOfPlayers.length;
+        }
+        this.allData = function () {
+            var country = [];
+            var result = this.competition + ", " + this.listOfBettingPlaces.length + " betting places, " + this.numbersOfPlayers + " bets" + "\n";
+            this.listOfBettingPlaces.forEach(function (place) {
+                result += "\t" + place.getMethod() + "\n"; 
+                place.listOfPlayers.forEach(function (player) {
+                    result += "\t\t" + player.getMethod() + "\n";
+                    country.push(player.country.countryName);
+                })  
+            });
+            var split = mostFrequent(country).split(",");
+            if (split[1] === 1) {
+                return result += "All countries occur equally in Betting House.";
+            }
+            return result += "There are " + split[1] + " bets on " + split[0];
+
+        }
+    };
+
+
+
+    //object factory
+
+    function createPlayer (person, bet, country) {
+        return new Player(person, bet, country);
+    };
+
+    function createBettingPlace(address) {
+        return new BettingPlace(address);
+    };
+
+
+    //utility functions
+
+    function abbrevation (country) {
+        var vowels = ["a", "e", "i", "o", "u"];
+        var abbr = country[0];
+        for (var i = 1; i < country.length; i++) {
+            
+            if (!(vowels.includes(country[i].toLowerCase()))) {
+                abbr += country[i];
+            }
+        }
+        return abbr.toUpperCase().slice(0, 2);
+    }
+
+    function getAge(birthday) {
+        var today = new Date();
+        var birthDate = new Date(birthday);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var month = today.getMonth() - birthDate.getMonth();
+        if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    function mostFrequent (array) {
+        var theMost = 1;
+        var counter = 0;
+        var temp;
+        for (var i = 0; i < array.length; i++) {
+            for (var j = i; j <array.length; j++) {
+                if (array[i] === array[j]) {
+                    counter++;
+                } 
+                if (theMost < counter) {
+                    theMost = counter;
+                    temp = array[i];
+                }
+            }
+            counter = 0;
+        }
+        return temp + "," + theMost;
     }
 
 
+    //testing
+    
+    try {
+        var Continent = new Continent();
+        Object.freeze(Continent);
+        
+        //first player
+        var person1 = new Person("Pera", "Peric", "Feb 23 1992");
+        var country1 = new Country("Srbija", 1, Continent.EUROPE);
+        var player1 = createPlayer(person1, 1050, country1);
+        //second player
+        var person2 = new Person("Pera", "Peric", "Feb 23 1992");
+        var country2 = new Country("Srbija", 1, Continent.EUROPE);
+        var player2 = createPlayer(person2, 1050, country2);
+        //third player 
+        var person3 = new Person("Pera", "Peric", "Feb 23 1992");
+        var country3 = new Country("Germay", 1, Continent.EUROPE);
+        var player3 = createPlayer(person3, 1050, country3);
+        //forth player
+        var person4 = new Person("Pera", "Peric", "Feb 23 1992");
+        var country4 = new Country("Srbija", 1, Continent.EUROPE);
+        var player4 = createPlayer(person4, 1050, country4);
+
+        //first betting place
+        var address1 = new Address ("Srbija", "Beograd", 11000, "Nemanjina", 41)
+        var bettingPlace1 = createBettingPlace(address1);
+        //second betting place
+        var address2 = new Address("Srbija", "Beograd", 11000, "Nemanjina", 34);
+        var bettingPlace2 = createBettingPlace(address2);
+
+        //add players to betting places
+        bettingPlace1.addPlayer(player1);
+        bettingPlace1.addPlayer(player2);
+        
+        bettingPlace2.addPlayer(player3);
+        bettingPlace2.addPlayer(player4);
+
+
+        //add betting places 
+        var bettingHouse = new BettingHouse("Football World Cup Winner,");
+        bettingHouse.addBettingPlaces(bettingPlace1);
+        bettingHouse.addBettingPlaces(bettingPlace2);
+
+        
+        console.log(bettingHouse.allData());
+
+        
+    }catch (err) {
+        console.log(err.message);
+    }
+})();
 
 
 
 
 
 
-})
+
---
+++ OOP/shoppingList.js
@@ -0,0 +1,81 @@
+"use strict";
+
+(function shoppingList() {
+
+    function Product (name, price, expDate) {
+
+        if (!name || !price || expDate){
+            throw new Error ("All field are required")
+        }
+
+        this.productID = parseInt(((99999 - 10000) * Math.random()) + 10000);
+        this.productName = name;
+        this.productPrice = price.toFixed(2);
+        this.expirationDate = new Date(expDate);
+
+        this.getInfo = function() {
+
+            var pName = this.productName;
+            var string = (pName.charAt(0).concat(pName.charAt(pName.length - 1))).toUpperCase();
+            var letterID = string + this.productID;
+
+            return letterID + ", " + pName + ", " +this.productPrice;
+        }
+    }
+
+
+    function ShoppingBag() {
+
+        this.productList = [];
+        this.addProduct = function (product) {
+
+            if (product.expirationDate.getTime() > Date.now()) {
+                this.productList.push(product);
+                return "Your product has valid expiration date!";
+            } else {
+                return "Your product has expired!!!";
+            }
+        }  
+
+        this.averagePrice = function () {
+
+            var average = this.calulateTotalPrice() / this.productList.length;
+            return average.toFixed(3);
+
+        }
+
+        this.getMostExpensive = function () {
+            var max = -Infinity;
+            var maxProduct;
+
+            this.productList.forEach (function(el) {
+                var currentPrice = parseFloat(el.productPrice);
+                 if (currentPrice > max) {
+                     max = currentPrice;
+                     maxProduct = e.getInfo();
+                 }
+            })
+            return maxProduct;
+        }
+
+        this.calulateTotalPrice = function () {
+            var priceOfAllProducts = 0;
+            this.productList.forEach (function (el) {
+                var productPrice = parseFloat(e.productPrice);
+                priceOfAllProducts += productPrice;
+            })
+            return priceOfAllProducts;
+        } 
+    }
+
+    function PaymentCard (balance, status, validDate) {
+        if (!balance || !status || !validDate) {
+            throw new Error ("All fields are required!")
+        }
+        this.accountBalance = balance.toFixed(2);
+        this.status = status;
+        this.valid = new Date (validDate);
+    }
+
+    
+}) 
